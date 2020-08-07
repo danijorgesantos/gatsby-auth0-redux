f6931fe26e3bb44368f3d8567c8139a25c4f9d25
Daniel Santos <daniel.santos@rupealgroup.onmicrosoft.com>
2020-08-07 14:38:38 +0100

    as

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,6 @@ import '../app.css'
 export default () => (
   <div className="center-me-auth0 column">
     <img src={uni} className="img-size-big" alt="logo" />
-    <Link className="slick-button" to="/account">Go to your account</Link>
+    <Link className="slick-button" to="/account">Go to your  sa  aassa account</Link>
   </div>
 )
